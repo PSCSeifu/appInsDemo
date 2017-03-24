752ba5f63cf9c36a4580594443de5a704c02a149
SEIFU TOLOSA <seifu@pscpayroll.com>
2017-03-24 11:21:40 +0000

    2nd commit

---
+++ ms.appIns.API/Controllers/ContactController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ms.appIns.Types.Contact;
+using Microsoft.Extensions.Logging;
+using Raven.Client.Document;
+
+namespace ms.appIns.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contact")]
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        //Get api/Contact
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = Data();
+            _logger.LogInformation("Fetched Contact list { @Contacts}", result);          
+            return Json(result);
+           
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            _logger.LogInformation($"Fetching a contact with id ={id}");
+            var result = Data().Where(c => c.Id == id).Select(c => c).SingleOrDefault();
+            _logger.LogInformation($"After Fetching a contact with id ={id}", result);
+            return Json(result);
+        }
+
+
+
+        private List<ContactDTO> Data()
+        {
+            return new List<ContactDTO> {   new ContactDTO { Id = 1, Address1 = "11 Cambridge Way", Address2 = "Cambridge", PostCode = "CB1 1WW", Age = 25 },
+               new ContactDTO  { Id = 2, Address1 = "12 Havehill Way", Address2 = "Haverhill", PostCode = "CB21 1WW", Age = 35 },
+                  new ContactDTO  { Id = 3, Address1 = "12 Barhill Way", Address2 = "Barhill", PostCode = "CB21 1WW", Age = 45 },
+                   new ContactDTO  { Id = 4, Address1 = "122 Chesterton Road", Address2 = "Haverhill", PostCode = "CB1 9GH", Age = 38 },
+                new ContactDTO  { Id = 5, Address1 = "88 Somthing Close", Address2 = "Barhill", PostCode = "CB2 3RR", Age = 22 }
+            };
+        }
+    }
+}
--- ms.appIns.API/Startup.cs
+++ ms.appIns.API/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Serilog;
 
 namespace ms.appIns.API
 {
@@ -20,6 +22,11 @@ namespace ms.appIns.API
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -27,15 +34,42 @@ namespace ms.appIns.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services.          
+            services.AddApplicationInsightsTelemetry(Configuration);
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(options =>
+                {
+                    options.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Unexpected Error");
+                    });
+                });
+            }
+
+            //Logging
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"))
+                         .AddSerilog()
+                         .AddDebug();
+            //Security
+
+            //Azure Appliation Insights
+            app.UseApplicationInsightsRequestTelemetry();
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            // Custom Middleware
+
 
             app.UseMvc();
         }
